36d53fc6840fcb264ff2f1550c0b01a4892945a5
Davy <103334758+Descode1@users.noreply.github.com>
2025-08-07 18:53:54 +0200

    Add express and socket.io

--- server/index.js
+++ server/index.js
@@ -1,1 +1,25 @@
-import { createServer } from "vite";
+import express from "express";
+import http from "http";
+import cors from "cors";
+import { Server } from "socket.io";
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin: "http://localhost:5173",
+        methods: ["GET","POST"]
+    }
+});
+
+io.on("connection",(socket)=>{
+    console.log("A user is connected");
+});
+app.get("/",(req,res)=>{
+    res.send("Hello World")
+})
+server.listen("3000",()=>{
+    console.log("listening on port 3000")
+})
