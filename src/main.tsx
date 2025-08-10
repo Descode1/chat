748a433eaa500ce6e2e4ee7b104f55ea33740978
Davy <103334758+Descode1@users.noreply.github.com>
2025-08-10 17:50:26 +0200

    Add users table for authorisation

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter, Routes,Route } from 'react-router'
+import SignUp from './pages/SignUp.tsx'
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById('root')!).render( 
   <BrowserRouter>
     <Routes>
       <Route path='/' element= {<App/>}/>
+      <Route path='/login' element = {<SignUp/>}/>
     </Routes>
   </BrowserRouter>,
 )
---
+++ src/pages/SignUp.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import supabase from "../../lib/supabase_client";
+
+
+function SignUp() {
+  const [username, setUsername] = useState<string>("");
+
+  const handleRegister = async (e:React.FormEvent) => {
+    e.preventDefault();
+    const { error } = await supabase.from("chat_users").insert([{username}]);
+    if(error){
+      console.error("Error: " + error);
+    } else {
+      console.log("Insert successfully");
+      setUsername("");
+    }
+  };
+  return (
+    <>
+    <form onSubmit={handleRegister}>
+      <input
+      placeholder="Enter Username"
+      type="text"
+      value={username}
+      onChange={(e)=>setUsername(e.target.value)}
+      />
+      <button>sign up</button>
+    </form>
+    </>
+  );
+}
+
+export default SignUp;
